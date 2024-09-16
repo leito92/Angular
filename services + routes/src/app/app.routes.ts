cd4d935900af6b4695f992e892092744f061aab2
Leonardo J. Ibáñez <leoo.davinci@gmail.com>
2024-09-16 18:42:49 -0300

    clear

--- services + routes/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductComponent } from './product/product.component';
-
-export const routes: Routes = [
-    { path:'product/:id', component: ProductComponent}
-];
--- tp_final/src/app/types/types.ts
+++
@@ -1,5 +0,0 @@
-export interface product {
-    id: string,
-    name: string,
-    info: any[]
-}
